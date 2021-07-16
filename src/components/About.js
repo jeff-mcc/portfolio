adb03b0df06f6addc7edb1caaf4c8b960785f8cf
jeff-mcc <reffery@gmail.com>
2021-07-15 23:45:16 -0600

    built out the projects component

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,13 @@ const About = () => {
     return(
         <div className="body">
             I am your father
-            <h3 className="subtitle">Inside of the lair you find...</h3>
+            <h3 className="subtitle">Inside the lair you find...</h3>
             <h1 className="title">Chickens, and children, and running shoes... but no goats.</h1>
             <div className="line"/>
-            {/* <div className="container"> */}
+            <div className="container">
                 <img className="familyImg" src="https://storage.googleapis.com/biomechweb/family%20pics-0379.jpg" alt="family"></img>
-            {/* </div> */}
-            <p className="textBox">I believe that in order to truly know a person you must understand what motivates them. So, what motivates me? First and foremost, I love learning, and I am passionate about health and the human body. For this reason I chose to study Biomechanics in grad school, and I thoroughly enjoyed making computer models of the human body.<br/><br/>But... then came the question following graduation, what next? For many that answer is simple, but unfortunately for me it wasn't. Eventually I decided to take a web development bootcamp, and the rest is history.<br/><br/>Outside of work, I have an awesome family which I love, a bunch of chickens (zero goats), and I find enjoyment as I run with my kids through the streets (and trails) of my home town.</p>
+                <p className="textBox">I believe that in order to truly know a person you must understand what motivates them. So, what motivates me? First and foremost, I love learning, and I am passionate about health and the human body. For this reason I chose to study Biomechanics in grad school, and I thoroughly enjoyed making computer models of the human body.<br/><br/>But... then came the question following graduation, what next? For many that answer is simple, but unfortunately for me it wasn't. Eventually I decided to take a web development bootcamp, and the rest is history. Outside of work, I have an awesome family which I love, a bunch of chickens (zero goats), and I find enjoyment as I run with my kids through the streets (and trails) of my home town.</p>
+            </div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,32 @@
-const Projects = () => {
+const Projects = (props) => {
+
+    // const handleLink = (url) => {
+    //     props.history.push(url)
+    // }
+    // onClick={()=>handleLink("https://biomechweb.com/")}
+
     return(
         <div className="body">
             Projects
+            <h3 className="subtitle">Etched on the walls you find...</h3>
+            <h1 className="title">Nothing, Jeffrey uses Git for version control, not stone walls</h1>
+            <div className="line"/>
+            <div className="container">
+                <div className="linked-container">
+                    {/* <div className="img-shape"> */}
+                        <a href="https://biomechweb.com/"><img className="project" src="https://storage.googleapis.com/biomechweb/steven-lelham-atSaEOeE8Nk-unsplash.jpg" alt="running on track"></img></a>
+                    {/* </div> */}
+                    <a href="https://biomechweb.com/"><h3 className="project-title">BiomechWeb</h3></a>
+                    <p className="project-text">A personal project that I created while enrolled at Devmountain. The site takes raw x and y coordinate data used in Biomechanics research, and displays calculated angles following processing. I used React to build the site, D3 to chart the displayed data, SASS/SCSS to manage styling, and NodeJS/PostgreSQL for the server and database.</p>
+                </div>
+                <div className="linked-container">
+                    <div className="img-shape">
+                        <a href="https://cache-n-dash.com/"><img className="project" src="https://storage.googleapis.com/biomechweb/photo-1505778276668-26b3ff7af103.jpeg" alt="geo-caching"></img></a>
+                    </div>
+                    <a href="https://cache-n-dash.com/"><h3 className="project-title">Cache-N-Dash</h3></a>
+                    <p className="project-text">A group project that I created as part of a remote team of four students while enrolled at Devmountain. We used React to build the site, the Google Maps API to render the map and geo-locations, and NodeJS/PostgreSQL for the server and database. I took the lead role on development for the database, server, and building front end React components that incorporated back end data. I also played a large role in Map debugging and integration, as well as site styling.</p>
+                </div>
+            </div>
         </div>
     )
 }
