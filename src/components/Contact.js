d4a499a1afef62bbb14715cf85111703dfd8c2db
jeff-mcc <reffery@gmail.com>
2021-07-19 13:16:00 -0600

    continued work on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,22 +9,25 @@ const Contact = () => {
             <h3 className="subtitle">You uncover a crystal ball. Upon peering into it you see...</h3>
             <h1 className="title">Numbers and letters that facilitate communication. Is this magic?</h1>
             <div className="line"/>
-            <div className="container">
-                <div>
-                    <GiScrollQuill />
-                    <p>reffery@gmail.com</p>
+            <div className="contact-container">
+                <div className="row-container">
+                    <GiScrollQuill className="stack-icon contact-icon"/>
+                    <p className="contact-text">reffery@gmail.com</p>
                 </div>
-                <div>
-                    <SiLinkedin />
-                    <p>LinkedIn profile</p>
-                </div>
-                <div>
-                    <SiGithub />
-                    <p>GitHub profile</p>
-                </div>
+                <a className="contact-link" href="https://www.linkedin.com/in/jeffrey-mcclellan-phd/"><div className="row-container">
+                    <SiLinkedin className="stack-icon contact-icon"/>
+                    <p className="contact-text">./in/jeffrey-mcclellan-phd/</p>
+                </div></a>
+                <a className="contact-link" href="https://github.com/jeff-mcc"><div className="row-container">
+                    <SiGithub className="stack-icon contact-icon"/>
+                    <p className="contact-text">github.com/jeff-mcc</p>
+                </div></a>
             </div>
         </div>
     )
 }
 
 export default Contact
+
+// https://www.linkedin.com/in/jeffrey-mcclellan-phd/
+// https://github.com/jeff-mcc
