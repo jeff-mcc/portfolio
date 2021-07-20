67b86b186d4d5b2c987477c37649aafe4fb50b42
jeff-mcc <reffery@gmail.com>
2021-07-20 16:56:53 -0600

    added server index.js to allow for specified server port for running on nginx droplet with multiple projects

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
+const {SERVER_PORT} = process.env;
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static(__dirname + '/../build'))
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'../build/index.html'))
+})
+
+app.listen(SERVER_PORT,()=>console.log(`Server is eating tacos on port: ${SERVER_PORT}`))
